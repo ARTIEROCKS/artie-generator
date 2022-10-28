2d653272c8731a00093ac85b2fcc10770fbca701
Luis Eduardo Imbernón <imbernon@gmail.com>
2022-10-28 18:37:33 +0200

    Added new ActionTests

--- src/test/java/artie/pedagogicalintervention/generator/test/ActionTest.java
+++ src/test/java/artie/pedagogicalintervention/generator/test/ActionTest.java
@@ -18,6 +18,8 @@ public class ActionTest {
     private Action eyesSelection1;
     private Action toneOfVoiceSelection1;
 
+    private Action voiceSpeedSelection1;
+
     @BeforeEach
     void setUp() throws Exception {
 
@@ -26,6 +28,11 @@ public class ActionTest {
                 .value("happy")
                 .valence("positive")
                 .grade("high")
+                .build();
+        Element emotionHappyWithoutGrade = Element.builder()
+                .name("emotion")
+                .value("happy")
+                .valence("positive")
                 .build();
         Element eyesGreen = Element.builder()
                 .name("eyes")
@@ -39,6 +46,15 @@ public class ActionTest {
                 .valence("encourage")
                 .grade("high")
                 .build();
+        Element voiceSpeed = Element.builder()
+                .name("voiceSpeed")
+                .value("high")
+                .valence("encourage")
+                .build();
+        Element toneOfVoiceSelectionFunction = Element.builder()
+                .name("toneOfVoiceSelection")
+                .build();
+
 
         ActionInput eyesSelection1xInput = ActionInput.builder()
                 .variablesNames(PrologStringList.builder().list(Arrays.asList("X")).build())
@@ -69,6 +85,20 @@ public class ActionTest {
                 .name("toneOfVoiceSelection")
                 .actionInputs(Arrays.asList(toneOfVoiceSelection1xyInput, toneOfVoiceSelection1xyzInput, toneOfVoiceSelection1yzInput))
                 .build();
+
+
+        ActionInput voiceSpeedSelection1wxzInput = ActionInput.builder()
+                .variablesNames(PrologStringList.builder().list(Arrays.asList("W", "X", "Z")).build())
+                .elements(Arrays.asList(voiceSpeed, emotionHappyWithoutGrade))
+                .build();
+        ActionInput voiceSpeedSelection1xyzInput = ActionInput.builder()
+                .variablesNames(PrologStringList.builder().list(Arrays.asList("X", "Y", "Z")).build())
+                .elements(Arrays.asList(toneOfVoiceSelectionFunction))
+                .build();
+        voiceSpeedSelection1 = Action.builder()
+                .name("voiceSpeedSelection")
+                .actionInputs(Arrays.asList(voiceSpeedSelection1wxzInput, voiceSpeedSelection1xyzInput))
+                .build();
     }
 
     @Test
@@ -86,6 +116,8 @@ public class ActionTest {
     @Test
     void getPrologFunctionTest(){
         assertEquals("eyeSelection(X, Y) :- high(positive(emotion(X))), high(positive(eyes(Y))).", eyesSelection1.getPrologFunction());
+        assertEquals("toneOfVoiceSelection(X, Y, Z) :- high(encourage(toneOfVoice(X, Y))), high(positive(emotion(X, Y, Z))), high(positive(eyes(Y, Z))).", toneOfVoiceSelection1.getPrologFunction());
+        assertEquals("voiceSpeedSelection(W, X, Z, Y) :- encourage(voiceSpeed(W, X, Z)), positive(emotion(W, X, Z)), toneOfVoiceSelection(X, Y, Z).", voiceSpeedSelection1.getPrologFunction());
     }
 
 }
